ea3b8d8cc1a95ef75cff180868f390097f13e8db
CactusDad <buoya88mosli88@gmail.com>
2023-12-22 18:03:30 +0100

    refactor: redirection of google & 42 auth

--- api/apps/gateway/src/microservices/auth/graphql/queries/gw.auth.query.resolver.ts
+++ api/apps/gateway/src/microservices/auth/graphql/queries/gw.auth.query.resolver.ts
@@ -1,9 +1,9 @@
-import { Resolver,Query, Args, Context} from '@nestjs/graphql';
+import { Resolver, Query, Args, Context } from '@nestjs/graphql';
 
 import { GQLUserModel } from 'apps/gateway/src/models';
 import { AuthCredentialsInput } from '../input';
 import { GatewayService, UserService } from '../../services';
-import {    UseGuards } from '@nestjs/common';
+import { UseGuards } from '@nestjs/common';
 import { GqlJwtAuthGuard } from '../../guards/gql.accessToken.guard';
 
 
@@ -11,36 +11,34 @@ import { GqlJwtAuthGuard } from '../../guards/gql.accessToken.guard';
 @Resolver()
 export class AuthQueryResolver {
     constructor(
-	private readonly gatewayService: GatewayService,
-	private readonly userService: UserService
-    ) {}
-    
-    
+        private readonly gatewayService: GatewayService,
+        private readonly userService: UserService
+    ) { }
+
+
     @Query(() => String)
     async helloT(): Promise<string> {
-      return "hello world from server";
+        return "hello world from server";
     }
-    
+
     @Query(() => GQLUserModel)
-    async findUserById(@Args('userId') userId: number) : Promise<GQLUserModel>{
-	const user = await this.userService.findById(userId);
-	return (user);
+    async findUserById(@Args('userId') userId: number): Promise<GQLUserModel> {
+        const user = await this.userService.findById(userId);
+        return (user);
     }
 
 
 
-    @Query(() => [GQLUserModel]) 
+    @Query(() => [GQLUserModel])
     async findAllUsers(): Promise<GQLUserModel[]> {
-	const users: GQLUserModel[] = await this.userService.findAll();
-	return users;
+        const users: GQLUserModel[] = await this.userService.findAll();
+        return users;
     }
 
     @UseGuards(GqlJwtAuthGuard)
     @Query(() => GQLUserModel)
-    async getUser(@Context() { req }): Promise<GQLUserModel>{
-     
-       const user = await this.userService.getMe(token);
-     const user = req.user;
+    async getUser(@Context() { req }): Promise<GQLUserModel> {
+        const user = req.user;
         return user;
     }
 
